7c51178614e9114a5e3799e344669f6c6cffefd4
vsulli <60162352+vsulli@users.noreply.github.com>
2024-10-05 15:48:22 -0500

    Had semicolon after submenu while statement. Preventing it from accessing switch statement

--- CipherEncoderDecoder/CipherEncoderDecoder.cpp
+++ CipherEncoderDecoder/CipherEncoderDecoder.cpp
@@ -9,6 +9,7 @@
 
 int submenu() {
     int selection2 = 0;
+
     std::cout << "Would you like to encode or decode a message?" << std::endl
         << "1) Encode" << std::endl
         << "2) Decode" << std::endl
@@ -16,27 +17,26 @@ int submenu() {
 
     std::cin >> selection2;
 
-    while (selection2 != -2);
-    {
+    while (selection2 != -2){
         switch (selection2) {
-        case 1:
-            break;
+            case 1:
+                break;
 
-        case 2:
-            break;
+            case 2:
+                break;
 
-        case 3:
-            selection2 = -2;
-            break;
+            case 3:
+                selection2 = -2;
+                break;
 
-        default:
-            std::cout << "Invalid Selection. Select a valid menu option" << std::endl;
-            std::cout << "Would you like to encode or decode a message?" << std::endl
-                << "1) Encode" << std::endl
-                << "2) Decode" << std::endl
-                << "3) Quit" << std::endl;
-            std::cin >> selection2;
-        }
+            default:
+                std::cout << "Invalid Selection. Select a valid menu option" << std::endl;
+                std::cout << "Would you like to encode or decode a message?" << std::endl
+                    << "1) Encode" << std::endl
+                    << "2) Decode" << std::endl
+                    << "3) Quit" << std::endl;
+                std::cin >> selection2;
+            }
     }
     return selection2; 
 }
@@ -56,34 +56,33 @@ void menu()
     while (selection != -1) {
         switch (selection) {
 
-        case 1:
-            // Atbash - encodes message with the reverse of the alphabet
-            selection = submenu();
-            break;
+            case 1:
+                // Atbash - encodes message with the reverse of the alphabet
+                selection = submenu();
+                break;
 
-        case 2:
-            break;
+            case 2:
+                break;
 
-        case 3:
-            break;
+            case 3:
+                break;
 
-        case 4:
-            break;
+            case 4:
+                break;
 
-        case 5:
-            selection = -1;
-            break;
+            case 5:
+                selection = -1;
+                break;
 
-        default:
-            std::cout << "Please select from the options below." << std::endl
-                << "1) Atbash Cipher" << std::endl
-                << "2) Caesar Cipher" << std::endl
-                << "3) Affine Cipher" << std::endl
-                << "4) Vigenere Cipher" << std::endl
-                << "5) Exit" << std::endl;
-            std::cin >> selection;
-        }
-
+            default:
+                std::cout << "Please select from the options below." << std::endl
+                    << "1) Atbash Cipher" << std::endl
+                    << "2) Caesar Cipher" << std::endl
+                    << "3) Affine Cipher" << std::endl
+                    << "4) Vigenere Cipher" << std::endl
+                    << "5) Exit" << std::endl;
+                std::cin >> selection;
+            }
     }
     return;
    
