634b4b51018737dea798d61346a691dd7863f762
vsulli <60162352+vsulli@users.noreply.github.com>
2024-10-05 14:31:19 -0500

    Agregar archivos de proyecto.

---
+++ CipherEncoderDecoder/CipherEncoderDecoder.cpp
@@ -0,0 +1,13 @@
+/* CipherEncoderDecoder.cpp
+* vsulli
+* 5 October 2024
+* Program that allows the user to determine the type of cipher
+* encode a message or decode a message based on that cipher type.
+*/
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
